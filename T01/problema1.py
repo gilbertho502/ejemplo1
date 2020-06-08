551baa468b2a1a26beb9abc39ffeb1c6cc9ab266
usac201503776 <2965479210101@ingeniera.usac.edu.gt>
2020-06-08 13:53:38 -0600

    Erro de ubucacin

--- T01/problema1.py
+++
@@ -1,8 +0,0 @@
-dato = int(input("ingresa un numero: "))
-for i in range(1, dato + 1):
-    x = 0
-    for j in range(1, (i // 2) + 1):
-        if ((i % j) == 0):
-             x = x + j;
-    if (x == i):
-        print("El numero %s en la lista de numeros, es perfecto" %i)
